4fbff41185acaceb9adb62200a38084410d80fa7
miglehp <migle.hp@gmail.com>
2023-06-03 18:50:52 +0200

    arreglos menores clases de bootstrap y formateo de ficheros con prettier

--- js/funciones.js
+++ js/funciones.js
@@ -1,56 +1,79 @@
 const clickCrearFiltrar = () => {
-    domFiltros.classList.toggle('visually-hidden');
-    domCrearTareas.classList.toggle('visually-hidden');
-  };
-  
-  const toggleCrearFiltrar = (event) => {
-    if (event.target === domToggleCrear) {
-      event.target.disabled = true;
-      domToggleFiltro.disabled = false;
-      clickCrearFiltrar();
-    } else if (event.target === domToggleFiltro) {
-      event.target.disabled = true;
-      domToggleCrear.disabled = false;
-      clickCrearFiltrar();
+  domFiltros.classList.toggle('visually-hidden');
+  domCrearTareas.classList.toggle('visually-hidden');
+};
+
+const toggleCrearFiltrar = (event) => {
+  if (event.target === domToggleCrear) {
+    event.target.disabled = true;
+    domToggleFiltro.disabled = false;
+    clickCrearFiltrar();
+  } else if (event.target === domToggleFiltro) {
+    event.target.disabled = true;
+    domToggleCrear.disabled = false;
+    clickCrearFiltrar();
+  }
+};
+
+const colorPicker = (prioridadStr) => {
+  switch (prioridadStr) {
+    case 'urgente':
+      return 'bg-danger-subtle';
+    case 'diaria':
+      return 'bg-warning-subtle';
+    case 'mensual':
+      return 'bg-info-subtle';
+  }
+};
+
+const buildTarea = (tarea) => {
+  let color = colorPicker(tarea.priority);
+
+  let li = document.createElement('li');
+  li.classList.add('list-group-item', color);
+
+  let inputRad = document.createElement('input');
+  inputRad.classList.add('form-check-input', 'me-1');
+  inputRad.type = 'checkbox';
+  inputRad.value = '';
+  inputRad.id = tarea.id;
+
+  let label = document.createElement('label');
+  label.classList.add('form-check-label');
+  label.textContent = tarea.title;
+
+  li.appendChild(inputRad);
+  li.appendChild(label);
+
+  return li;
+};
+
+const printTareas = (arrTareas) => {
+  let ulHtml = domListaTareas;
+  ulHtml.innerHTML = '';
+
+  arrTareas.forEach((tarea) => ulHtml.appendChild(buildTarea(tarea)));
+};
+
+const filtrarPorPrioridad = (event) => {
+  let prioridad = event.target.value;
+  if (prioridad !== '') {
+    let listaFiltrada = tareas.filter((tarea) => tarea.priority === prioridad);
+    printTareas(listaFiltrada);
+  } else {
+    printTareas(tareas);
+  }
+};
+
+const filtrarPorNombre = (event) => {
+  let title = domFiltroSearch.value.toLowerCase();
+  if (event.key === 'Enter' || event.pointerId === 1) {
+    event.preventDefault();
+    if (title !== '') {
+      let listaFiltrada = tareas.filter((tarea) => tarea.title.toLowerCase().includes(title));
+      printTareas(listaFiltrada);
+    } else {
+      printTareas(tareas);
     }
-  };
-  
-  const colorPicker = (prioridadStr) => {
-    switch (prioridadStr) {
-      case 'urgente':
-        return 'bg-danger-subtle';
-      case 'diaria':
-        return 'bg-warning-subtle';
-      case 'mensual':
-        return 'bg-info-subtle';
-    }
-  };
-  
-  const buildTarea = (tarea) => {
-    let color = colorPicker(tarea.priority);
-  
-    let li = document.createElement('li');
-    li.classList.add('list-group-item', color);
-  
-    let inputRad = document.createElement('input');
-    inputRad.classList.add('form-check-input', 'me-1');
-    inputRad.type = 'checkbox';
-    inputRad.value = '';
-    inputRad.id = tarea.id;
-  
-    let label = document.createElement('label');
-    label.classList.add('form-check-label');
-    label.textContent = tarea.title;
-  
-    li.appendChild(inputRad);
-    li.appendChild(label);
-  
-    return li;
-  };
-  
-  const printTareas = (arrTareas) => {
-    let ulHtml = domListaTareas;
-    ulHtml.innerHTML = '';
-  
-    arrTareas.forEach((tarea) => ulHtml.appendChild(buildTarea(tarea)));
-  };
+  }
+};
--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ const domToggleFiltro = document.querySelector('#toggleFiltro');
 const domCrearTareas = document.querySelector('#crearForm');
 const domFiltros = document.querySelector('#filtros');
 const domListaTareas = document.querySelector('#listaTareas');
-
-
+const domSelectFiltro = document.querySelector('#selectFiltro');
+const domFiltroSearch = document.querySelector('#filtroSearch');
+const domFiltroSearchBtn = document.querySelector('#filtroSearchBtn');
 
 printTareas(tareas);
 
 domToggleCrear.addEventListener('click', toggleCrearFiltrar);
 domToggleFiltro.addEventListener('click', toggleCrearFiltrar);
+
+domSelectFiltro.addEventListener('change', filtrarPorPrioridad);
+domFiltroSearch.addEventListener('keydown', filtrarPorNombre);
+domFiltroSearchBtn.addEventListener('click', filtrarPorNombre);
